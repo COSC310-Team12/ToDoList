96844e6c67ddf03d5575a681e3668bb750c479a7
te-sa <teresa_saller@freenet.de>
2022-10-07 23:27:16 -0700

    Implemented basic ToDo class to store text, date, and whether the task is done

---
+++ app/src/main/java/com/example/todolist/ToDo.java
@@ -0,0 +1,42 @@
+package com.example.todolist;
+
+public class ToDo {
+    private String text;
+    private boolean done;
+    private String date;
+
+    public ToDo(String text) {
+        this.text = text;
+    }
+
+    public ToDo(String text, boolean done, String date) {
+        this.text = text;
+        this.done = done;
+        this.date = date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
+
