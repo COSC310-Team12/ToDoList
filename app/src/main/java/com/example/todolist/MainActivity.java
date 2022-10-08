5297a7db51d98d71a67220f69d56654f6600e302
te-sa <teresa_saller@freenet.de>
2022-10-08 00:15:51 -0700

    added capability for user to enter to-do's

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -13,29 +16,41 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<ToDo> toDoList;
     private RecyclerView recyclerView;
+    private RecyclerAdapter recyclerAdapter;
+    private EditText inputToDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
-
         toDoList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        inputToDo = findViewById(R.id.inputToDo);
+
+        Button submitButton = findViewById(R.id.submitButton);
 
         setAdapter();
         setToDos();
+
+        submitButton.setOnClickListener(view -> {
+            // adding user input to-do to array list
+            toDoList.add(new ToDo(inputToDo.getText().toString()));
+            // need to call this so UI updates and newly added item is displayed
+            recyclerAdapter.notifyItemInserted(recyclerAdapter.getItemCount());
+        });
     }
 
     private void setToDos() {
+        // may implement with database later on
         toDoList.add(new ToDo("Finish prototype"));
         toDoList.add(new ToDo("Improve design"));
-        toDoList.add(new ToDo("Understand RecyclerViews"));
-        toDoList.add(new ToDo("Add more to-do's"));
     }
 
     private void setAdapter() {
-        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(toDoList);
+        // boiler-plate code
+        recyclerAdapter = new RecyclerAdapter(toDoList);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
