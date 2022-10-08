3922009b0ed66674a6048b0703a8a3f87426764f
te-sa <teresa_saller@freenet.de>
2022-10-07 23:37:29 -0700

    added some test to-do's to RecyclerView

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<ToDo> toDoList;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        toDoList = new ArrayList<>();
+
+        setAdapter();
+        setToDos();
+    }
+
+    private void setToDos() {
+        toDoList.add(new ToDo("Finish prototype"));
+        toDoList.add(new ToDo("Improve design"));
+        toDoList.add(new ToDo("Understand RecyclerViews"));
+        toDoList.add(new ToDo("Add more to-do's"));
+    }
+
+    private void setAdapter() {
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(toDoList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(recyclerAdapter);
     }
 }
