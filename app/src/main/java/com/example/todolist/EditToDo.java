c4f8534a4631990340527c5fa4ce0ae2b5da36d3
te-sa <teresa_saller@freenet.de>
2022-10-11 14:35:50 -0700

    made changes to layout and added test code to EditToDo

--- app/src/main/java/com/example/todolist/EditToDo.java
+++ app/src/main/java/com/example/todolist/EditToDo.java
@@ -9,6 +9,7 @@ public class EditToDo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_task);
+        setContentView(R.layout.activity_edit_todo);
+        setTitle("Edit to-do: "+getIntent().getStringExtra("text"));
     }
 }
